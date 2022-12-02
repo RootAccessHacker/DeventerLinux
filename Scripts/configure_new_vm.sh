9f4b2eab21d32bd31926029856bc3e3ea4f3d624
Mr-XOOMY <rolandregtop@gmail.com>
2022-12-02 13:46:12 +0100

    network config added

--- Scripts/configure_new_vm.sh
+++ Scripts/configure_new_vm.sh
@@ -13,5 +13,28 @@ sudo sed -i "s|$currentHostname|$newHostname|g" /etc/hostname
 # Reset machine-id
 sudo truncate -s 0 /etc/machine-id
 
+# Configure network settings
+ls /etc/sysconfig/network-scripts/
+echo "Which config file would you like to configure?"
+read -r networkConfig
+
+echo -n "IPv4 address: "
+read -r ipaddr
+
+echo -n "prefix: "
+read -r prefix
+
+echo -n "Gateway: "
+read -r gateway
+
+echo -n "DNS1: "
+read -r dns1
+
+sudo sed -i "s|ONBOOT=*|ONBOOY=yes|g" /etc/sysconfig/network-scripts/$networkConfig
+sudo sed -i "s|IPADDR=*|IPADDR=$ipaddr|g" /etc/sysconfig/network-scripts/$networkConfig
+sudo sed -i "s|PREFIX=*|PREFIX=$prefix|g" /etc/sysconfig/network-scripts/$networkConfig
+sudo sed -i "s|GATEWAY=*|GATEWAY=$gateway|g" /etc/sysconfig/network-scripts/$networkConfig
+sudo sed -i "s|DNS1=*|DNS1=$dns1|g" /etc/sysconfig/network-scripts/$networkConfig
+
 # Reboot server
 sudo reboot now

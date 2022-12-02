4f033fb562d4241f1c6622148e2d3838f810479a
Mr. XOOMY <rolandregtop@gmail.com>
2022-12-02 10:10:20 +0100

    configure new vm script

---
+++ Scripts/configure_new_vm.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Get current hostname
+currentHostname=$(hostname)
+
+# Ask new hostname
+echo -n "What should the hostname of this machine be?: "
+read -r newHostname
+
+# Set new hostname
+sudo sed -i "s|$currentHostname|$newHostname|g" /etc/hostname
+
+# Reset machine-id
+sudo truncate -s 0 /etc/machine-id

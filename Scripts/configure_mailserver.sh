3fa5aad33238fe820ae6f32435d261941dd99284
Cerberus <myrmidon@protonmail.ch>
2022-12-05 21:08:47 +0100

    Update configure_mailserver.sh

--- Scripts/configure_mailserver.sh
+++ Scripts/configure_mailserver.sh
@@ -1,28 +1,39 @@
 #!/usr/bin/env bash
 
-echo "Enter host IP: "
+
+echo "Enter host IP:"
 read -r ipaddr
 
-echo "Enter host host-/domain name:"
-read -r hdname
 
-echo "Enter domain: "
+echo "Enter hostname:"
+read -r hname
+
+
+echo "Enter domain name:"
 read -r domain
+
 
 # remove interfering services
 sudo dnf remove sendmail* -y
 
 # set hostname
 
+
 # install postfix
 sudo dnf install postfix -y
+
 
 # install mailx client
 sudo dnf install mailx -y
 
+
 # change hosts file 
+sudo echo "$ipaddr	$hname $hname.$domain" >> /etc/hosts
+
 sudo sed -i "s|#myhostname = host.domain.tld|myhostname = $hdname|g" /etc/postfix/main.cf
 sudo sed -i "s|#mydomain = domain.tld|mydomain = $domain|g" /etc/postfix/main.cf
+sudo sed -i "s|#inet_interfaces = all|inet_interfaces = all|g" /etc/postfix/main.cf
+
 
 sudo sed -i "s|#mydomain = domain.tld|mydomain = $domain|g" /etc/postfix/main.cf
 

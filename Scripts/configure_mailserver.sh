82f78b9bb3afac0a3279df42a748758373b30d80
Cerberus <myrmidon@protonmail.ch>
2022-12-06 14:12:05 +0100

    Update configure_mailserver.sh

--- Scripts/configure_mailserver.sh
+++ Scripts/configure_mailserver.sh
@@ -30,14 +30,12 @@ sudo dnf install mailx -y
 # change hosts file 
 sudo echo "$ipaddr	$hname $hname.$domain" >> /etc/hosts
 
+
 sudo sed -i "s|#myhostname = host.domain.tld|myhostname = $hname|g" /etc/postfix/main.cf
 sudo sed -i "s|#mydomain = domain.tld|mydomain = $domain|g" /etc/postfix/main.cf
 sudo sed -i "s|#inet_interfaces = all|inet_interfaces = all|g" /etc/postfix/main.cf
-
-
+sudo sed -i "s|#home_mailbox = Maildir/|home_mailbox = Maildir/|g" /etc/postfix/main.cf 
 sudo sed -i "s|#mydomain = domain.tld|mydomain = $domain|g" /etc/postfix/main.cf
-
-
 
 
 #sudo systemctl reboot

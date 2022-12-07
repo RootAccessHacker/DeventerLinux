28c1e7693b1a89f312562591f8571077d0b32629
Cerberus <myrmidon@protonmail.ch>
2022-12-07 17:26:37 +0100

    Update configure_mailserver.sh

--- Scripts/configure_mailserver.sh
+++ Scripts/configure_mailserver.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+#sudo nmcli networking off
+#sudo nmcli networking on
 
 echo "Enter host IP:"
 read -r ipaddr
@@ -12,12 +14,14 @@ read -r hname
 echo "Enter domain name:"
 read -r domain
 
+echo "Enter networks: [(1) and (2) 127.0.0.0/8]"
+read -r networks
 
 # remove interfering services
 sudo dnf remove sendmail* -y
 
-# set hostname
-
+# change hostname.domain
+sudo hostnamectl set-hostname $hname.$domain
 
 # install postfix
 sudo dnf install postfix -y
@@ -26,26 +30,29 @@ sudo dnf install postfix -y
 # install mailx client
 sudo dnf install mailx -y
 
-
-# change hostname.domain
-sudo hostnamectl set-hostname $hname.$domain
+# make copy of default config
+sudo cp /etc/postfix/main.cf /etc/postfix/main.cf.default
 
 # change hosts file 
-sudo echo "$ipaddr	$hname $hname.$domain" >> /etc/hosts
+sudo echo "$ipaddr $hname.$domain" >> /etc/hosts
 
 
 sudo sed -i "s|#myhostname = host.domain.tld|myhostname = $hname|g" /etc/postfix/main.cf
 sudo sed -i "s|#mydomain = domain.tld|mydomain = $domain|g" /etc/postfix/main.cf
 sudo sed -i "s|inet_interfaces = localhost|inet_interfaces = all|g" /etc/postfix/main.cf
-
 sudo sed -i "s|#home_mailbox = Maildir/|home_mailbox = Maildir/|g" /etc/postfix/main.cf 
-sudo sed -i "s|#mydomain = domain.tld|mydomain = $domain|g" /etc/postfix/main.cf
+
+# new 1
+sudo sed -i "s|#myorigin = $mydomain|myorigin = \$mydomain|g" /etc/postfix/main.cf
+sudo sed -i "286i mynetworks = $networks"
+
+
 
 
 # start services
 sudo systemctl start postfix
 sudo systemctl enable postfix
 sudo postfix reload
+exec bash
 
-
-sudo systemctl reboot
+#sudo systemctl reboot

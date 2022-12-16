4de39ea326ee8ee36ef4392226f6d0ae0ea2424e
Haylee <hayleedrenth@gmail.com>
2022-12-16 12:32:53 +0100

    Create install_squid_sarg.sh

---
+++ Scripts/install_squid_sarg.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+
+# Install squid, sarg and apache2
+sudo apt install squid sarg  apache2 -y
+
+# Remove default index.html
+sudo rm /var/www/html/index.html
+
+# Backup default squid.conf
+squidConf=/etc/squid/squid.conf.bup
+if [ ! -f "$squidConf" ]; then
+        sudo mv /etc/squid/squid.conf $squidConf
+else
+        written=false
+        counter=1
+        while ! $written; do
+                if [ ! -f "$squidConf-$counter" ]; then
+                        sudo mv /etc/squid/squid.conf "$squidConf-$counter"
+                        written=true
+                else
+                        ((counter+=1))
+                fi
+        done
+fi
+
+# Backup default sarg.conf
+sargConf=/etc/sarg/sarg.conf.bup
+if [ ! -f "$sargConf" ]; then
+        sudo mv /etc/sarg/sarg.conf $sargConf
+else
+        written=false
+        counter=1
+        while ! $written; do
+                if [ ! -f "$sargConf-$counter" ]; then
+                        sudo mv /etc/sarg/sarg.conf "$sargConf-$counter"
+                        written=true
+                else
+                        ((counter+=1))
+                fi
+        done
+fi
+
+# Create squid config file
+sudo -i <<-EOF
+echo -e "acl localhost src 127.0.0.1
+acl vlan1101 src 10.0.0.0/24
+http_access allow localhost
+http_access allow vlan1101
+http_port 0.0.0.0:3129
+http_access deny all" | sudo tee "/etc/squid/squid.conf"
+EOF
+
+# Create sarg config file
+sudo -i <<-EOF
+echo -e "access_log /var/log/squid/access.log
+graphs yes
+output_dir /var/www/html
+resolve_ip yes
+date_format e
+overwrite_reports yes
+long_url no" | sudo tee "/etc/sarg/sarg.conf"
+EOF
+
+# Create sarg -x cronjob
+cronCommand="sarg -x"
+cronJob="*/1 * * * * $cronCommand"
+sudo crontab -l | grep -v -F "$cronJob" ; echo "$cronJob" | sudo crontab -
+
+# Enable squid service
+sudo systemctl enable --now squid
+sleep 5
+sudo netstat -antp | grep -E "squid"

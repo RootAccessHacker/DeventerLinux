0752f8c1c703d87621358b8e593421e16997f87f
Cerberus <myrmidon@protonmail.ch>
2022-12-23 14:54:54 +0100

    Update zabbix_installer.sh

--- Scripts/zabbix_installer.sh
+++ Scripts/zabbix_installer.sh
@@ -2,21 +2,17 @@
 
 
 sudo dnf install mysql-server -y
-sudo systemctl start mysqld.service
 sudo systemctl enable mysqld.service --now
-
-/usr/bin/
 
 sudo rpm -Uvh https://repo.zabbix.com/zabbix/6.2/rhel/8/x86_64/zabbix-release-6.2-3.el8.noarch.rpm
 sudo dnf clean all 
 
-echo "\nPackages pulled\n"
 
 sudo dnf module switch-to php:7.4 
-sudo dnf install zabbix-server-mysql -y zabbix-web-mysql -y zabbix-apache-conf -y zabbix-sql-scripts -y zabbix-selinux-policy -y zabbix-agent -y
+sudo dnf install zabbix-server-mysql zabbix-web-mysql zabbix-apache-conf zabbix-sql-scripts zabbix-selinux-policy zabbix-agent
 
 
 
-sudo systemctl restart zabbix-server.service zabbix-agent.service php-fpm.service 
-sudo systemctl enable zabbix-server zabbix-agent httpd php-fpm 
+#sudo systemctl restart zabbix-server.service zabbix-agent.service php-fpm.service 
+#sudo systemctl enable zabbix-server zabbix-agent httpd php-fpm --now
 

513fa42ae662c58c42b18d89469ab424ddad7c21
Mr-XOOMY <rolandregtop@gmail.com>
2023-01-13 16:24:57 +0100

    .

--- Scripts/configure_proxy.sh
+++ Scripts/configure_proxy.sh
@@ -4,17 +4,17 @@
 checkConfig="# Proxy ad.harderwijk.local"
 
 if ! grep -Fxq "$checkConfig" /etc/environment; then
-    sudo -i <<-EOF
-    echo -e "# Proxy ad.harderwijk.local
-    http_proxy=\"http:\/\/10.0.0.14:3129\/\"
-    https_proxy=\"http:\/\/10.0.0.14:3129\/\"
-    ftp_proxy=\"http:\/\/10.0.0.14:3129\/\"
-    no_proxy=\"localhost,127.0.0.1,localaddress,.localdomain.com\/\"
-    HTTP_PROXY=\"10.0.0.14:3129\/\"
-    HTTPS_PROXY=\"10.0.0.14:3129\/\"
-    FTP_PROXY=\"10.0.0.14:3129\/\"
-    NO_PROXY=\"localhost,127.0.0.1,localaddress,.localdomain.com\/\"" | sudo tee -a /etc/environment 1> /dev/null
-    EOF
+	sudo -i <<-EOF
+	echo "# Proxy ad.harderwijk.local
+	http_proxy="http://10.0.0.4:3129/"
+	https_proxy="http://10.0.0.4:3129/"
+	ftp_proxy="http://10.0.0.4:3129/"
+	no_proxy="localhost,127.0.0.1,localaddress,.localdomain.com/"
+	HTTP_PROXY="10.0.0.4:3129/"
+	HTTPS_PROXY="10.0.0.4:3129/"
+	FTP_PROXY="10.0.0.4:3129/"
+	NO_PROXY="localhost,127.0.0.1,localaddress,.localdomain.com/"" | sudo tee -a /etc/environment 1> /dev/null
+	EOF
 else
-    echo "Proxy settings already configured"
+	echo "Proxy settings already configured"
 fi

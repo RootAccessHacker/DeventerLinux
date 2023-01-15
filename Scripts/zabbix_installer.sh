db696997845dba3bd0a6cc68f003bae0d569fb27
Cerberus <myrmidon@protonmail.ch>
2023-01-15 02:11:17 +0100

    Update zabbix_installer.sh

--- Scripts/zabbix_installer.sh
+++ Scripts/zabbix_installer.sh
@@ -4,17 +4,17 @@
 sudo dnf install mysql-server -y
 sudo systemctl enable mysqld.service --now
 
-# y, n, y, y
+# n, enter pass, y, n, y, y
 /usr/bin/mysql_secure_installation
 
 sudo rpm -Uvh https://repo.zabbix.com/zabbix/6.2/rhel/8/x86_64/zabbix-release-6.2-3.el8.noarch.rpm
 sudo dnf clean all 
 
-
 sudo dnf module switch-to php:7.4
 sudo dnf install zabbix-server-mysql zabbix-web-mysql zabbix-apache-conf zabbix-sql-scripts zabbix-selinux-policy zabbix-agent -y
 
-# Create Mysql Database
+
+## Create Mysql Database
 # mysql -uroot -p
 # password
 
@@ -24,10 +24,15 @@ sudo dnf install zabbix-server-mysql zabbix-web-mysql zabbix-apache-conf zabbix-
 # mysql> set global log_bin_trust_function_creators = 1;
 # mysql> quit;
 
-# sudo zcat /usr/share/zabbix-sql-scripts/mysql/server.sql.gz | mysql --default-character-set=utf8mb4 -uzabbix -p zabbix 
+mysql -uroot -p -e "create database zabbix character set utf8mb4 collate utf8mb4_bin; create user zabbix@localhost identified by 'password'; grant all privileges on zabbix.* to zabbix@localhost; set global log_bin_trust_function_creators = 1;"
 
+sudo zcat /usr/share/zabbix-sql-scripts/mysql/server.sql.gz | mysql --default-character-set=utf8mb4 -uzabbix -p zabbix
 
-#sudo systemctl restart zabbix-server.service zabbix-agent.service php-fpm.service 
-#sudo systemctl enable zabbix-server zabbix-agent httpd php-fpm --now
+sudo sed -i "20i DBPassword=password" /etc/zabbix/zabbix_server.conf
 
-# https://github.com/RootAccessHacker/DeventerLinux.git
+sudo systemctl enable zabbix-server zabbix-agent httpd php-fpm --now
+
+sudo systemctl restart zabbix-server.service zabbix-agent.service php-fpm.service 
+
+sudo firewall-cmd --add-service=http --zone=public --permanent
+sudo firewall-cmd --reload

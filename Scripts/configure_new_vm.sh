9bcdac511066cd0bd5ba0d6839e5d4e43aab61df
Mr-XOOMY <rolandregtop@gmail.com>
2023-01-16 09:30:53 +0100

    Merged code of proxy into new_vm script

--- Scripts/configure_new_vm.sh
+++ Scripts/configure_new_vm.sh
@@ -58,6 +58,25 @@ if [ "$operatingSystem" == "ubuntu" ]; then
 	" | sudo tee "/etc/netplan/$networkConfig"
 	EOF
 
+	# Check if proxy settings are already in /etc/enviroment
+	checkConfig="# Proxy ad.harderwijk.local"
+	if ! grep -Fxq "$checkConfig" /etc/environment; then
+		# Configure proxy settings
+		sudo -i <<-EOF
+		echo "# Proxy ad.harderwijk.local
+		http_proxy="http://10.0.0.4:3129/"
+		https_proxy="http://10.0.0.4:3129/"
+		ftp_proxy="http://10.0.0.4:3129/"
+		no_proxy="localhost,127.0.0.1,localaddress,.localdomain.com/"
+		HTTP_PROXY="10.0.0.4:3129/"
+		HTTPS_PROXY="10.0.0.4:3129/"
+		FTP_PROXY="10.0.0.4:3129/"
+		NO_PROXY="localhost,127.0.0.1,localaddress,.localdomain.com/"" | sudo tee -a /etc/environment 1> /dev/null
+		EOF
+	else
+		echo "Proxy settings already configured"
+	fi
+
 else
 	# Configure network settings
 	networkConfig="ifcfg-$adapterName"
